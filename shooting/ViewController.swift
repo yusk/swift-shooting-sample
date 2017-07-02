66c1dd12ac9c594f99c7b833962b77b5fd68e1e5
yusk <tdfagamdb186@yahoo.co.jp>
2017-07-02 15:33:55 +0900

    Initial Commit

---
+++ shooting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shooting
+//
+//  Created by yusk on 2017/07/02.
+//  Copyright © 2017年 yusk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
